d27d9c2b6f2c6c1c4cccc5cc2f67a358503367c8
unknown <romanovainnap@yandex.ru>
2023-03-17 13:23:51 +0300

    block landing school

--- src/components/School/School.jsx
+++ src/components/School/School.jsx
@@ -1,23 +1,52 @@
 import React from "react";
 import './School.css';
 import vector from '../../images/Vector.svg';
+import scrollLeft from '../../images/arrow_left.svg';
+import scrollRight from '../../images/arrow_right.svg';
+import imageMan from '../../images/photo_person_man.svg';
+import imageWoman from '../../images/photo_person_woman.svg';
 
 function School() {
 
     return (
         <section className="school">
+            <div className="school__rocket">
             <div className="school__block">
                 <h2 className="education__title">Наша <span className="education__title-span">школа</span></h2>
-                <p className="become__subtitle">В своём стремлении повысить качество жизни, они забывают,
+                <p className="school__subtitle">В своём стремлении повысить качество жизни, они забывают,
                     что перспективное планирование играет важную роль в формировании системы обучения кадров,
                     соответствующей насущным потребностям. </p>
-                <ul className="education__list">
+                <ul className="school__list">
                     <li className="education__list-item"><img src={vector} className="education__list-image" />1000+ выпускников</li>
                     <li className="education__list-item"><img src={vector} className="education__list-image" />80% трудоустроились</li>
                 </ul>
             </div>
-            <div className="school__fon" />
-            <div className="school__image" />
+            
+            <div className="school__staff">
+                <div className="school__left"><a href="#" className="school__scroll">
+                    <img className="school__scroll-image" alt="стрелка" src={scrollLeft} /></a></div>
+                <ul className="school__user">
+                    <li className="school__user-item">
+                        <img className="school__image" src={imageMan} />
+                        <div className="school__content">
+                        <h4 className="school__name">Иван Иванов</h4>
+                        <p className="school__post">Senior Android Developer. Работал в Amazon Web services, Alibaba Group, Яндекс.</p>
+                        </div>
+                    </li>
+                    <li className="school__user-item">
+                        <img className="school__image" src={imageWoman} />
+                        <div className="school__content">
+                        <h4 className="school__name">Анна Иванова</h4>
+                        <p className="school__post">Руководитель группы по работе с рекламными агенствами Вконтакте.
+                            Работала со Сбером, Яндексом, М-видио.</p>
+                            </div>
+                    </li>
+                </ul>
+
+                <div className="school__left"><a href="#" className="school__scroll">
+                    <img className="school__scroll-image" alt="стрелка" src={scrollRight} /></a></div>
+            </div>
+            </div>
         </section>
     )
 }
