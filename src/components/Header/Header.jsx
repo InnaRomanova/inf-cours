daf3a378552d98acccdeef7513602912644a8825
unknown <romanovainnap@yandex.ru>
2023-04-08 21:32:21 +0300

    header and components adaptiv

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,55 @@
 import React from "react";
-import './Header.css';
-import logo from '../../../src/images/Logo.png';
-import icon from '../../images/icon_arrow.svg';
-import telegram from '../../images/icon-telegram.svg';
+import "./Header.css";
+import logo from "../../../src/images/Logo.png";
+import icon from "../../images/icon_arrow.svg";
+import telegram from "../../images/icon-telegram.svg";
+import { useMediaQuery } from "usehooks-ts";
 
 function Header() {
-    return (
+  const matches = useMediaQuery("(max-width: 1250px)");
+  return (
+    <>
+      {matches ? (
         <header className="header">
-            <img src={logo} className="header__logo" alt="Логотип"/>
-            <ul className="header__menu">
-                <li className="header__menu-item">Курсы
-                    <img className="header__menu-icon" src={icon} /></li>
-                <li className="header__menu-item"><a href="#" className="header__menu-link" target="_blank">О компании</a></li>
-                <li className="header__menu-item"><a href="#" className="header__menu-link" target="_blank">Акции</a></li>
-                <li className="header__menu-item"><a href="#" className="header__menu-link" target="_blank">Контакты</a></li>
-
-                <li className="header__menu-item"><p className="header__contact">+375(33) 637-30-79</p></li>
-                <li className="header__menu-item"><a href="#" className="header__telegram">
-                    <img className="header__telegram-icon" src={telegram} /></a></li></ul>
-
+          <img src={logo} className="header__logo" alt="Логотип" />
+          <link className="header__menu-burger"></link>
         </header>
-    )
+      ) : (
+        <header className="header">
+          <img src={logo} className="header__logo" alt="Логотип" />
+          <ul className="header__menu">
+            <li className="header__menu-item">
+              Курсы
+              <img className="header__menu-icon" src={icon} />
+            </li>
+            <li className="header__menu-item">
+              <a href="#" className="header__menu-link" target="_blank">
+                О компании
+              </a>
+            </li>
+            <li className="header__menu-item">
+              <a href="#" className="header__menu-link" target="_blank">
+                Акции
+              </a>
+            </li>
+            <li className="header__menu-item">
+              <a href="#" className="header__menu-link" target="_blank">
+                Контакты
+              </a>
+            </li>
+
+            <li className="header__menu-item">
+              <p className="header__contact">+375(33) 637-30-79</p>
+            </li>
+            <li className="header__menu-item">
+              <a href="#" className="header__telegram">
+                <img className="header__telegram-icon" src={telegram} />
+              </a>
+            </li>
+          </ul>
+        </header>
+      )}
+    </>
+  );
 }
 export default Header;
