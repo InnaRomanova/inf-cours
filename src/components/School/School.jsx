d4d2748f6e3279f9d65bfd0c377c5c2cabff8864
unknown <romanovainnap@yandex.ru>
2023-06-12 13:11:00 +0300

    edit component Shool

--- src/components/School/School.jsx
+++ src/components/School/School.jsx
@@ -6,33 +6,96 @@ import scrollLeft from "../../images/arrow_left.svg";
 import scrollRight from "../../images/arrow_right.svg";
 import imageMan from "../../images/photo_person_man.svg";
 import imageWoman from "../../images/photo_person_woman.svg";
+import CarouselSlider from "react-carousel-slider";
 
 function School() {
   const matches = useMediaQuery("(max-width: 1350px)");
   const user = document.querySelector(".school__scroll");
   const [list, setList] = useState(false);
 
-  function handleList() {
-    setList(!list);
-    document.addEventListener("click", function () {
-      return arr.userMan;
-    });
-  }
-
-  const arr = {
-    userWoman: {
+  const arrReviews = [
+    {
       id: 1,
       image: imageWoman,
       name: "Анна Иванова",
       text: "Руководитель группы по работе с рекламными агенствами  Вконтакте. Работала со Сбером, Яндексом, М-видео.",
     },
-    userMan: {
+    {
       id: 2,
       image: imageMan,
       name: "Иван Иванов",
       text: "Senior Android Developer. Работал в Amazon Web services, Alibaba Group, Яндекс.",
     },
+    {
+      id: 3,
+      image: imageMan,
+      name: "Иван Иванов 2",
+      text: "Senior Android Developer. Работал в Amazon Web services, Alibaba Group, Яндекс.",
+    },
+  ];
+
+  let itemsStyle = {
+    margin: "0 41px 0 0",
+    padding: "0",
+    display: "flex",
   };
+
+  let rBtnCpnt = (
+    <div className="school__right">
+      <a href="#" className="school__scroll school__scroll_right">
+        <img className="school__scroll-image" alt="стрелка" src={scrollRight} />
+      </a>
+    </div>
+  );
+
+  let lBtnCpnt = (
+    <div className="school__left">
+      <a href="#" className="school__scroll school__scroll_left">
+        <img className="school__scroll-image" alt="стрелка" src={scrollLeft} />
+      </a>
+    </div>
+  );
+
+  const reviews = [];
+
+  arrReviews.forEach((rewiew, index) => {
+    reviews.push(
+      <>
+        <div className="school__user-item">
+          <img className="school__image" src={rewiew.image} />
+          <div className="school__content">
+            <h4 className="school__name">{rewiew.name}</h4>
+            <p className="school__post">{rewiew.text}</p>
+          </div>
+        </div>
+      </>
+    );
+  });
+
+  let reviewsCard = (
+    <CarouselSlider
+      sliderBoxStyle={{
+        height: "450px",
+        width: "80%",
+      }}
+      accEle={{ dots: false }}
+      slideCpnts={reviews}
+      itemsStyle={itemsStyle}
+      buttonSetting={{ placeOn: "middle-outside" }}
+      rBtnCpnt={rBtnCpnt}
+      lBtnCpnt={lBtnCpnt}
+    />
+  );
+
+  /*
+  function handleList() {
+    setList(!list);
+    document.addEventListener("click", function () {
+      return arrReviews.userMan;
+    });
+  }
+  */
+
   //   function handleOpen(e) {
   //     e.preventDefault();
 
@@ -41,139 +104,41 @@ function School() {
 
   return (
     <>
-      {matches ? (
-        <section className="school">
-          <div className="school__rocket">
-            <div className="school__block">
-              <h2 className="education__title">
-                Наша <span className="education__title-span">школа</span>
-              </h2>
-              <p className="school__subtitle">
-                В своём стремлении повысить качество жизни, они забывают, что
-                перспективное планирование играет важную роль в формировании
-                системы обучения кадров, соответствующей насущным потребностям.
-              </p>
-              <ul className="school__list">
-                <li className="education__list-item">
-                  <img src={vector} className="education__list-image" />
-                  1000+ выпускников
-                </li>
-                <li className="education__list-item">
-                  <img src={vector} className="education__list-image" />
-                  80% трудоустроились
-                </li>
-              </ul>
-            </div>
+      <section className="school">
+        <div className="school__rocket">
+          <div className="school__block">
+            <h2 className="education__title">
+              Наша <span className="education__title-span">школа</span>
+            </h2>
+            <p className="school__subtitle">
+              В своём стремлении повысить качество жизни, они забывают, что
+              перспективное планирование играет важную роль в формировании
+              системы обучения кадров, соответствующей насущным потребностям.
+            </p>
+            <ul className="school__list">
+              <li className="education__list-item">
+                <img src={vector} className="education__list-image" />
+                1000+ выпускников
+              </li>
+              <li className="education__list-item">
+                <img src={vector} className="education__list-image" />
+                80% трудоустроились
+              </li>
+            </ul>
+          </div>
 
-            <div className="school__staff">
-              <div className="school__left">
-                <a
-                  href="#"
-                  className="school__scroll school__scroll_left"
-                  onClick={handleList}
-                >
-                  <img
-                    className="school__scroll-image"
-                    alt="стрелка"
-                    src={scrollLeft}
-                  />
-                </a>
-              </div>
-              <ul className="school__user">
-                <li className="school__user-item">
-                  <img className="school__image" src={imageWoman} />
-                  <div className="school__content">
-                    <h4 className="school__name">{arr.userWoman.name}</h4>
-                    <p className="school__post">{arr.userWoman.text}</p>
-                  </div>
-                </li>
-              </ul>
-
-              <div className="school__left">
-                <a
-                  href="#"
-                  className="school__scroll school__scroll_right"
-                  onClick={handleList}
-                >
-                  <img
-                    className="school__scroll-image"
-                    alt="стрелка"
-                    src={scrollRight}
-                  />
-                </a>
+          <div className="school__staff">
+            <div className="school__user">
+              <div style={{ position: "relative", margin: "0 auto" }}>
+                {reviewsCard}
               </div>
             </div>
           </div>
-        </section>
-      ) : (
-        <section className="school">
-          <div className="school__rocket">
-            <div className="school__block">
-              <h2 className="education__title">
-                Наша <span className="education__title-span">школа</span>
-              </h2>
-              <p className="school__subtitle">
-                В своём стремлении повысить качество жизни, они забывают, что
-                перспективное планирование играет важную роль в формировании
-                системы обучения кадров, соответствующей насущным потребностям.
-              </p>
-              <ul className="school__list">
-                <li className="education__list-item">
-                  <img src={vector} className="education__list-image" />
-                  1000+ выпускников
-                </li>
-                <li className="education__list-item">
-                  <img src={vector} className="education__list-image" />
-                  80% трудоустроились
-                </li>
-              </ul>
-            </div>
-
-            <div className="school__staff">
-              <div className="school__left">
-                <a href="#" className="school__scroll school__scroll_left">
-                  <img
-                    className="school__scroll-image"
-                    alt="стрелка"
-                    src={scrollLeft}
-                  />
-                </a>
-              </div>
-              <ul className="school__user">
-                <li className="school__user-item">
-                  <img className="school__image" src={arr.userMan.image} />
-                  <div className="school__content">
-                    <h4 className="school__name">{arr.userMan.name}</h4>
-                    <p className="school__post">{arr.userMan.text}</p>
-                  </div>
-                </li>
-
-                <li className="school__user-item">
-                  <img className="school__image" src={arr.userWoman.image} />
-                  <div className="school__content">
-                    <h4 className="school__name">{arr.userWoman.name}</h4>
-                    <p className="school__post">{arr.userWoman.text}</p>
-                  </div>
-                </li>
-              </ul>
-
-              <div className="school__left">
-                <a
-                  href="#"
-                  className="school__scroll school__scroll_right"
-                  onClick={handleList}
-                >
-                  <img
-                    className="school__scroll-image"
-                    alt="стрелка"
-                    src={scrollRight}
-                  />
-                </a>
-              </div>
-            </div>
+          <div className="school__staff">
+            <div className="school__user">{reviews}</div>
           </div>
-        </section>
-      )}
+        </div>
+      </section>
     </>
   );
 }
