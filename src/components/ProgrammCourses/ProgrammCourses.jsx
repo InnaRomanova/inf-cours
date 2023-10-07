5516bac14812486c68216892a2db300206fcc8a6
unknown <romanovainnap@yandex.ru>
2023-10-07 16:12:40 +0300

    edit popup modal

--- src/components/ProgrammCourses/ProgrammCourses.jsx
+++ src/components/ProgrammCourses/ProgrammCourses.jsx
@@ -10,193 +10,188 @@ import softSkills from "../../images/SoftSkills.png";
 import "../Navigation/Navigation.css";
 
 function ProgrammCourses({ active, handleClose }) {
-  const matches = useMediaQuery("(min-width: 450px)");
+  const matches = useMediaQuery("(min-width: 550px)");
 
   console.log(matches);
   return (
     <>
       {matches ? (
-        <div
-          className={active ? "propgramm-mobile  active" : "propgramm"}
-          style={{ border: "1px solid red" }}
-        >
-          {/* <div className="propgramm__container" active={active}>
-            <div className="programm__block">
-              <h3 className="programm__title">Digital-маркетинг</h3>
-              <button
-                className="programm__close-button"
-                onClick={() => {
-                  console.log("hello");
-                }}
-              />
-              <p className="programm__text">
-                Курс по интернет-меркетингу, SMM, контекстной и таргетированной
-                рекламе, копирайту, SEO и PR. На курсах преподают ведущие
-                эксперты отрасли
-              </p>
-              <div className="program__list">
-                <div className="programm_list-item">
-                  <p className="programm_start">
-                    Старт: <span className="span">31 февраля</span>
-                  </p>
-                </div>
-                <div className="programm_list-item">
-                  <p className="programm_start">
-                    Длительность: <span className="span">24 месяца</span>
-                  </p>
-                </div>
-              </div>
-            </div>
-
-            <div className="programm__section">
-              <div className="programm__section-left">
-                <div className="programm__training">
-                  <h4 className="programm__training-title">
-                    Программа обучения
-                  </h4>
-                  <p className="programm__training-text">
-                    Вы узнаете, что такое интернет-маркетинг с точки зрения
-                    бизнеса и чего обычно ждет от маркетолога заказчик.
-                  </p>
-                </div>
-                <div className="programm__image">
-                  <a className="programm__link" href="#">
-                    <img
-                      className="programm__side"
-                      src={leftVector}
-                      alt="стрелка налево"
-                    />
-                  </a>
-                  <ul className="programm__image-list">
-                    <li className="programm__image-list_item">
-                      <img
-                        className="programm__icon"
-                        src={kouch}
-                        alt="картинка"
-                      />
-                    </li>
-                    <li className="programm__image-list_item">
-                      <img
-                        className="programm__icon"
-                        src={android}
-                        alt="картинка"
-                      />
-                    </li>
-                    <li className="programm__image-list_item">
-                      <img
-                        className="programm__icon"
-                        src={digital}
-                        alt="картинка"
-                      />
-                    </li>
-                    <li className="programm__image-list_item">
-                      <img
-                        className="programm__icon"
-                        src={softSkills}
-                        alt="картинка"
-                      />
-                    </li>
-                  </ul>
-
-                  <a className="programm__link" href="#">
-                    <img
-                      className="programm__side"
-                      src={rigthVector}
-                      alt="стрелка направо"
-                    />
-                  </a>
-                </div>
-
-                <div className="programm__marketing">
-                  <div className="programm__marketing-container">
-                    <h4 className="programm__marketing-title">
-                      Основы интернет-маркетинга
-                    </h4>
-                    <button className="programm__close-block" />
+        <div className={active ? "propgramm-mobile  active" : "propgramm"}>
+          {
+            <div className="propgramm__container" active={active}>
+              <div className="programm__block">
+                <h3 className="programm__title">Digital-маркетинг</h3>
+                <button
+                  className="programm__close-button"
+                  onClick={() => {
+                    handleClose();
+                  }}
+                />
+                <p className="programm__text">
+                  Курс по интернет-меркетингу, SMM, контекстной и
+                  таргетированной рекламе, копирайту, SEO и PR. На курсах
+                  преподают ведущие эксперты отрасли
+                </p>
+                <div className="program__list">
+                  <div className="programm_list-item">
+                    <p className="programm_start">
+                      Старт: <span className="span">31 февраля</span>
+                    </p>
                   </div>
-                  <ul className="marketing__list">
-                    <li className="marketing__item">
-                      - Основные концепции маркетинга (маркетинг и бизнес)
-                    </li>
-                    <li className="marketing__item">- Аудитория и инсайты</li>
-                    <li className="marketing__item">
-                      - Анализ рынка и конкурентов
-                    </li>
-                    <li className="marketing__item">
-                      - Бренды и позиционирование
-                    </li>
-                    <li className="marketing__item">
-                      - Карта пути клиента (CJM) и воронка продаж
-                    </li>
-                    <li className="marketing__item">
-                      - Retention маркетинг (e-mail, sms, push, pop-up,
-                      чат-боты)
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="programm__marketing section">
-                  <div className="programm__marketing-container">
-                    <h4 className="programm__marketing-title">
-                      Стратегия digital-продвижения
-                    </h4>
-                    <button className="programm__close-block programm__list_open" />
-                  </div>
-                </div>
-
-                <div className="programm__marketing">
-                  <div className="programm__marketing-container">
-                    <h4 className="programm__marketing-title">
-                      Сайты и приложения
-                    </h4>
-                    <button className="programm__close-block programm__list_open" />
-                  </div>
-                </div>
-
-                <div className="programm__marketing">
-                  <div className="programm__marketing-container">
-                    <h4 className="programm__marketing-title">
-                      Аналитика для интернет-маркетинга
-                    </h4>
-                    <button className="programm__close-block programm__list_open" />
+                  <div className="programm_list-item">
+                    <p className="programm_start">
+                      Длительность: <span className="span">24 месяца</span>
+                    </p>
                   </div>
                 </div>
               </div>
 
-              <div className="programm__section-right">
-                <div className="programm__right-container">
-                  <div className="programm__inf">
-                    <div className="become__circle become__circle_active">
-                      -24%
-                    </div>
-                    <div className="programm__month">
-                      <p className="programm__months">48 </p>
-                      <span className="programm__price">BYN / мес.</span>
-                    </div>
-                    <span className="become__price-number">
-                      Рассрочка на 24 месяца
-                    </span>
+              <div className="programm__section">
+                <div className="programm__section-left">
+                  <div className="programm__training">
+                    <h4 className="programm__training-title">
+                      Программа обучения
+                    </h4>
+                    <p className="programm__training-text">
+                      Вы узнаете, что такое интернет-маркетинг с точки зрения
+                      бизнеса и чего обычно ждет от маркетолога заказчик.
+                    </p>
                   </div>
-                  <div className="programm__discount">
-                    <div className="programm__discount-money">
-                      <h4 className="become__money-block">
-                        <del>2 387</del>
+                  <div className="programm__image">
+                    <a className="programm__link" href="#">
+                      <img
+                        className="programm__side"
+                        src={leftVector}
+                        alt="стрелка налево"
+                      />
+                    </a>
+                    <ul className="programm__image-list">
+                      <li className="programm__image-list_item">
+                        <img
+                          className="programm__icon"
+                          src={kouch}
+                          alt="картинка"
+                        />
+                      </li>
+                      <li className="programm__image-list_item">
+                        <img
+                          className="programm__icon"
+                          src={android}
+                          alt="картинка"
+                        />
+                      </li>
+                      <li className="programm__image-list_item">
+                        <img
+                          className="programm__icon"
+                          src={digital}
+                          alt="картинка"
+                        />
+                      </li>
+                      <li className="programm__image-list_item">
+                        <img
+                          className="programm__icon"
+                          src={softSkills}
+                          alt="картинка"
+                        />
+                      </li>
+                    </ul>
+
+                    <a className="programm__link" href="#">
+                      <img
+                        className="programm__side"
+                        src={rigthVector}
+                        alt="стрелка направо"
+                      />
+                    </a>
+                  </div>
+
+                  <div className="programm__marketing programm__marketing_active">
+                    <div className="programm__marketing-container">
+                      <h4 className="programm__marketing-title">
+                        Основы интернет-маркетинга
                       </h4>
-                      <span className="become__price">BYN </span>
+                      <button className="programm__close-block" />
                     </div>
-                    <div className="programm__monthly">
-                      <h3 className="become__monthly-block">1 984</h3>
-                      <span className="become__prices">BYN</span>
+                    <ul className="marketing__list">
+                      <li className="marketing__item">
+                        - Основные концепции маркетинга (маркетинг и бизнес)
+                      </li>
+                      <li className="marketing__item">- Аудитория и инсайты</li>
+                      <li className="marketing__item">
+                        - Анализ рынка и конкурентов
+                      </li>
+                      <li className="marketing__item">
+                        - Бренды и позиционирование
+                      </li>
+                      <li className="marketing__item">
+                        - Retention маркетинг (e-mail, sms, push, pop-up,
+                        чат-боты)
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div className="programm__marketing section">
+                    <div className="programm__marketing-container">
+                      <h4 className="programm__marketing-title">
+                        Стратегия digital-продвижения
+                      </h4>
+                      <button className="programm__close-block programm__list_open" />
+                    </div>
+                  </div>
+
+                  <div className="programm__marketing">
+                    <div className="programm__marketing-container">
+                      <h4 className="programm__marketing-title">
+                        Сайты и приложения
+                      </h4>
+                      <button className="programm__close-block programm__list_open" />
+                    </div>
+                  </div>
+
+                  <div className="programm__marketing">
+                    <div className="programm__marketing-container">
+                      <h4 className="programm__marketing-title">
+                        Аналитика для интернет-маркетинга
+                      </h4>
+                      <button className="programm__close-block programm__list_open" />
                     </div>
                   </div>
                 </div>
-                <button className="become__button-signup programm__btn-module">
-                  Записаться
-                </button>
+
+                <div className="programm__section-right">
+                  <div className="programm__right-container">
+                    <div className="programm__inf">
+                      <div className="become__circle become__circle_active programm__circle_active">
+                        -24%
+                      </div>
+                      <div className="programm__month">
+                        <p className="programm__months">48 </p>
+                        <span className="programm__price">BYN / мес.</span>
+                      </div>
+                      <span className="become__price-number">
+                        Рассрочка на 24 месяца
+                      </span>
+                    </div>
+                    <div className="programm__discount">
+                      <div className="programm__discount-money">
+                        <h4 className="become__money-block">
+                          <del>2 387</del>
+                        </h4>
+                        <span className="become__price">BYN </span>
+                      </div>
+                      <div className="programm__monthly">
+                        <h3 className="become__monthly-block">1 984</h3>
+                        <span className="become__prices">BYN</span>
+                      </div>
+                    </div>
+                  </div>
+                  <button className="become__button-signup programm__btn-module">
+                    Записаться
+                  </button>
+                </div>
               </div>
             </div>
-          </div> */}
-          HElP
+          }
         </div>
       ) : (
         <div
@@ -295,9 +290,6 @@ function ProgrammCourses({ active, handleClose }) {
                       - Бренды и позиционирование
                     </li>
                     <li className="marketing__item">
-                      - Карта пути клиента (CJM) и воронка продаж
-                    </li>
-                    <li className="marketing__item">
                       - Retention маркетинг (e-mail, sms, push, pop-up,
                       чат-боты)
                     </li>
@@ -335,7 +327,7 @@ function ProgrammCourses({ active, handleClose }) {
               <div className="programm__section-right">
                 <div className="programm__right-container">
                   <div className="programm__inf">
-                    <div className="become__circle become__circle_active">
+                    <div className="become__circle become__circle_active programm__circle_active">
                       -24%
                     </div>
                     <div className="programm__month">
